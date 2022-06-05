24cb07c84c56732387a4b41bf61d3590427eed1b
hikaru <sool.inc1109@gmail.com>
2022-06-05 17:10:05 +0900

    first commit

---
+++ lib/model/book_model.dart
@@ -0,0 +1,31 @@
+
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_basic/class/book.dart';
+
+class BookFirestore {
+  static final CollectionReference books =
+      FirebaseFirestore.instance.collection('book');
+
+ static Future<dynamic> getBookinfo() async {
+    List<Book> bookList = [];
+    try {
+      var snapshot = await books.get();
+      var docs = snapshot.docs;
+      for (int i = 0; i < docs.length; i++) {
+        Map<String, dynamic> data = docs[i].data() as Map<String, dynamic>;
+        Book book = Book(title: data['title'], author: data['author']);
+        bookList.add(book);
+      }
+      return bookList;
+      //futurebuilderのsnapshot.dataに飛んでいく
+    } catch (e) {
+      return null;
+      //futurebuilderのsnapshot.dataに飛んでいく
+    }
+
+  
+  }
+
+  
+}
